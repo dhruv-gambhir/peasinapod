a87cc0deb425ec7ff9ac73798b76c03979121cdd
Gambhir Dhruv <gambhirdhruv03@gmail.com>
2025-05-21 01:23:54 +0800

    add logo

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,7 @@
 export default function Sidebar() {
-    return <main className="bg-[#2E8B57]"></main>;
+    return <main className="flex justify-center bg-[#2E8B57]">
+        <div className="flex items-center justify-center w-1/2 h-1/12 rounded-full bg-[#F0F0F0]">
+            <img src="/logo.png" alt="Logo" className="w-1/2" />
+        </div>
+    </main>;
 }
